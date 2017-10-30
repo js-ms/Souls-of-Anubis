abb5d53faa960c915b7f1d3217e5ec7d62da08d6
jsebastianms1 <jsebastianms1@gmail.com>
2017-10-29 23:55:33 -0500

    hello
    hola primer commit

---
+++ PorfinLowPoly/Assets/MapEditor.cs
@@ -0,0 +1,19 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (DelaunayTerrain))]
+public class MapEditor : Editor {
+
+	public override void OnInspectorGUI ()
+	{
+		base.OnInspectorGUI ();
+
+		DelaunayTerrain map = target as DelaunayTerrain;
+
+		map.Generate();
+	}
+
+}
+*/
