728f4a3f76d7977611dafee073628654d91e1ff9
JulianCamiloLG <julycamy_125@hotmail.com>
2018-02-06 21:46:16 -0500

    sorpresa

---
+++ PorfinLowPoly/Assets/Scripts/mirarPersonaje.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirarPersonaje : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Linkle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (player.transform);
+	}
+}
---
+++ PorfinLowPoly/Assets/Scripts/misiones.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class misiones : MonoBehaviour {
+
+	private int tiempoCamara;
+	public GameObject text1;
+
+	// Use this for initialization
+	void Start () {
+		tiempoCamara = 0;
+		GetComponent<CharacterController> ().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine (esperarAnimacion());
+		moverCamera ();
+		tiempoCamara++;
+
+	}
+
+	IEnumerator esperarAnimacion(){
+		yield return new WaitForSeconds (5);
+		GetComponent<CharacterController> ().enabled = true;
+		text1.SetActive (true);
+	}
+
+	void moverCamera(){
+		GameObject camara = GameObject.Find ("VistaLinkle3ra");
+		if (tiempoCamara < 100) {
+			camara.transform.Rotate (Vector3.up * 20f * Time.deltaTime);
+		}
+		else if (tiempoCamara < 201) {
+			camara.transform.Rotate (-Vector3.up * 20f * Time.deltaTime);
+		}
+		else if (tiempoCamara < 302) {
+			camara.transform.Rotate (-Vector3.up * 20f * Time.deltaTime);
+		}
+		else if (tiempoCamara < 403) {
+			camara.transform.Rotate (Vector3.up * 20f * Time.deltaTime);
+		}
+	}
+}
