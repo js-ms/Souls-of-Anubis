51bf683691e2110dce1e917c3a475304b42e49a6
JulianCamiloLG <julycamy_125@hotmail.com>
2018-02-08 04:57:35 -0500

    Revert "analisis" df
    This reverts commit e55ec30409e1996432ffb5106dc7ffb3042fbac2.

--- PorfinLowPoly/Assets/Scripts/MainMenu.cs
+++ PorfinLowPoly/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	public void play(){
+	public void playGame(){
 		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex+1);
 	}
 }
