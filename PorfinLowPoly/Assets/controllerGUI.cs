e55ec30409e1996432ffb5106dc7ffb3042fbac2
jsebastianms1 <jsebastianms1@gmail.com>
2018-02-07 22:05:51 -0500

    analisis

---
+++ PorfinLowPoly/Assets/controllerGUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class controllerGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void jugar(){
+		Scene scene = SceneManager.GetSceneByName ("ProPro");
+		GameObject disp = GameObject.Find ("disparodef");
+		SceneManager.LoadScene ("ProPro");
+		SceneManager.MoveGameObjectToScene (disp,scene);
+	}
+}
